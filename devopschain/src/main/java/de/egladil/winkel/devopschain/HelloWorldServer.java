0abd3ecc1e5ca37ecc197e5ed9b706f3198a8d13
winkel <winkel@linux.fritz.box>
2019-09-06 19:41:47 +0200

    initial EchoServer

---
+++ devopschain/src/main/java/de/egladil/winkel/devopschain/HelloWorldServer.java
@@ -0,0 +1,12 @@
+package de.egladil.winkel.devopschain;
+
+/**
+* based on code of Martin Ombura Jr. <@martinomburajr>
+*/
+
+public class HelloWorldServer {
+	public static void main(String[] args) {
+		Server myserver = new Server();
+        myserver.connectToServer();
+    }
+}
